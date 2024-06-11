b1e09816e3e27366a57ca8e8e04b39c50d9e5614
richelbilderbeek <xiaoyan.long2@gmail.com>
2024-06-11 13:02:16 +0200

    mating 0.002

---
+++ c++/mate0.002.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#!!! adjust the time quota !!!
+
+#SBATCH --time=20:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=8000
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=single
+
+datadir="/pfs/work7/workspace/scratch/fr_xl1011-data/evosexrole_sexratio"
+./bin/evorole --force param=mate0.002.json rep=5 outdir=$example
+
